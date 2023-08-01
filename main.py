74d16d0c4c6d54d3085a5a73c704821cd6e89923
LiBinUtokyo <120563331+LiBinUtokyo@users.noreply.github.com>
2023-08-01 22:30:52 +0900

    基本完成了每次运行都监视东大官网ADMISSIONS部分的消息

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 本内容参考自网页https://cloud.tencent.com/developer/article/1932575
 用于学习网络爬虫
-
+从东京大学官网检测最新通知并存储起来，当有新通知的时候发送邮件
 '''
 import csv #用于把爬取的数据存成CSV格式
 import time #用于对请求加延时以避免被反爬
@@ -9,6 +9,8 @@ from time import sleep #同上
 import random #用于对延时设置随机数
 import requests #用于向网站发送请求
 from lxml import etree #网页解析库lxml
+import os
+import send_email #自定义函数，用于发送邮件
 
 #构造请求url和头部信息headers
 url = 'https://www.u-tokyo.ac.jp/ja/index.html'
@@ -23,25 +25,62 @@ html = response.text
 #解析网页，对html文本使用 etree.HTML(html)解析，得到Element对象
 parse = etree.HTML(html)
 
-#提取tr，对Element对象使用xpath筛选，返回一个列表（里面的元素也是Element）
+#提取news_web，对Element对象使用xpath筛选，返回一个列表（里面的元素也是Element）
 # all_tr = parse.xpath('//*[@id="content"]/div[4]/div/div/div[2]/div')
 #                     //*[@id="content"]/div[4]/div/div/div[2]/div/ul/li[1]/a/p[2]
-all_tr = parse.xpath('//*[@id="content"]/div[4]/div/div/div[2]/div/ul/li')
+news_web = parse.xpath('//*[@id="content"]/div[4]/div/div/div[2]/div/ul/li')
 
 # print(len(all_tr))
 
+#通过xpath获取标签内的text和herf（超链接）
+news = []
+for item in news_web:
+    news.append({
+        'date': ''.join(item.xpath('./a/p[1]/text()')).strip(),
+        'title': ''.join(item.xpath('./a/p[2]/text()')).strip(),
+        # 'link': ''.join(['https://www.u-tokyo.ac.jp/',tr.xpath('./a/@href')]).strip()
+        'link': 'https://www.u-tokyo.ac.jp'+item.xpath('./a/@href')[0]
+    })
 
-for tr in all_tr:
-    tr = {
-        'date': ''.join(tr.xpath('./a/p[1]/text()')).strip(),
-        'title': ''.join(tr.xpath('./a/p[2]/text()')).strip()
-    }
-    print(tr)
-    # print(tr.tag, tr[0][0].tag)
-    # print(tr.text, tr[0][0].text)
+print(news)
+
+#将news存到csv文件里
+# #判断news.txt文件是否存在，不存在则创建，并写入date-title-link数据
+# if not os.path.isfile('news.txt'):
+#     f = open(news.txt,'w')
+
+
+
+#     f.close()
+# #存在的话，提取里面的内容，如果内容不一致，则表明通知更新
+
+with open('news.csv','a+',encoding='utf_8_sig',newline='') as f:
+    # a+为读写模式
+    # utf_8_sig格式导出csv不乱码
+    # 从头到尾对比news.csv文件与爬到的文件是否有一致的条目，没有的话就添加并提示
+    fieldnames = ['date','title','link']
+    for i in range(len(news)):
+        flag = False
+        #设置文件读取指针到开头
+        f.seek(0,0)
+        reader = csv.DictReader(f)
+        for row in reader:
+            # print(type(row),row)
+            if news[i] == row:
+                flag = True
+        if not flag:
+            f.seek(0,2)
+            writer = csv.DictWriter(f,fieldnames)
+            writer.writerow(news[i])
+            send_email.send(news[i])
+            time.sleep(2)
 
 
 
 
+    # fieldnames = ['date','title','link']
+    # writer = csv.DictWriter(f,fieldnames)
+    # for i in range(len(news)):
+    #     writer.writerow(news[i])
 
 
--- send_email.py
+++ send_email.py
@@ -9,38 +9,40 @@ import smtplib
 from config import info
 
 
+def send(news):
+    #news 为 {date:...,title:...,link:...}格式的字典
+    #发送方邮箱信息
+    msg_from = info['msg_from']
+    passwd = info['passwd']
 
-#发送方邮箱信息
-msg_from = info['msg_from']
-passwd = info['passwd']
+    #接收方邮箱信息
+    # to = 'email adress'
+    to = info['to']
 
-#接收方邮箱信息
-# to = ['libin11170225@gmail.com','li@g.ecc.u-tokyo.ac.jp']
-to = info['to']
+    #邮件内容
+    # msg = MIMEMultipart() #MIMEMultipart类可以放各种各样的格式的内容
+    # cont = "弃我去者，昨日之日不可留; 乱我心者，今日之日多烦忧。"
+    # msg.attach(MIMEText(cont,'plain','utf-8')) #MIMEText三个参数，文本内容，文本格式和编码格式
 
-#邮件内容
-# msg = MIMEMultipart() #MIMEMultipart类可以放各种各样的格式的内容
-# cont = "弃我去者，昨日之日不可留; 乱我心者，今日之日多烦忧。"
-# msg.attach(MIMEText(cont,'plain','utf-8')) #MIMEText三个参数，文本内容，文本格式和编码格式
+    # cont = "弃我去者，昨日之日不可留; 乱我心者，今日之日多烦忧。"
+    cont = news['link']
+    msg = MIMEText(cont,'plain','utf-8')
 
-cont = "弃我去者，昨日之日不可留; 乱我心者，今日之日多烦忧。"
-msg = MIMEText(cont,'plain','utf-8')
+    #邮件主题
+    msg['Subject'] = '-'.join([news['date'],news['title']])
 
-#邮件主题
-msg['Subject'] = '宣州谢眺楼饯别校书叔云_李白'
-
-#from和to信息
-msg['From'] = msg_from
-msg['to'] = to
+    #from和to信息
+    msg['From'] = msg_from
+    msg['to'] = to
 
 
-try:
-    #通过SSL发送，需要定义服务器地址和端口
-    s = smtplib.SMTP_SSL('smtp.qq.com', 465)
-    #登陆邮箱
-    s.login(msg_from, passwd)
-    #开始发送
-    s.sendmail(msg_from,to,msg.as_string())
-    print('邮件发送成功')
-except smtplib.SMTPException as e:
-    print(' Error: 无法发送邮件.Case:%s'%e)
+    try:
+        #通过SSL发送，需要定义服务器地址和端口
+        s = smtplib.SMTP_SSL('smtp.qq.com', 465)
+        #登陆邮箱
+        s.login(msg_from, passwd)
+        #开始发送
+        s.sendmail(msg_from,to,msg.as_string())
+        print('邮件发送成功')
+    except smtplib.SMTPException as e:
+        print(' Error: 无法发送邮件.Case:%s'%e)
